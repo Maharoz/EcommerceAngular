f83a99694433c0df8fc7f7d32c2d690b4121d9b6
maharoz <mahruj66@gmail.com>
2020-06-20 21:11:47 +0600

    Adding first API controller

---
+++ Ecommerce/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetsProduct()
+        {
+            return "single product";
+        }
+    }
+}
