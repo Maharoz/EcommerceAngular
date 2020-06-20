3b8894f2decbc2b63bb02528242a87ae9624daf0
maharoz <mahruj66@gmail.com>
2020-06-20 21:31:53 +0600

    Setting up entity framework

---
+++ Ecommerce/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Controllers;
+using Ecommerce.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
