dd009d3c3ed4be5c22613c4d018cdd125203941f
maharoz <mahruj66@gmail.com>
2020-07-15 19:57:19 +0600

    showed data on ui

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {IProduct} from './models/product';
+import {IPagination} from './models/pagination';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Ecommerce';
+  products:any[];
+
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:44358/api/products?pageSize=50')
+    .subscribe((response:IPagination)=>{
+     // console.log(response);
+      this.products= response.data;
+    },error=>{
+      console.log(error);
+    })
+  }
+  
+  
 }
