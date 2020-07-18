8eaad4927cb6e20a5f0ca0795223457500adb3b2
maharoz <mahruj66@gmail.com>
2020-07-18 13:07:29 +0600

    Toastr message added

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import {ToastrModule} from 'ngx-toastr';
 
 
 
@@ -12,7 +13,11 @@ import { ServerErrorComponent } from './server-error/server-error.component';
   declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass:'toas-bottom-right',
+      preventDuplicates:true
+    })
   ],
   exports:[NavBarComponent]
 })
--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -8,10 +8,11 @@ import { Observable, throwError } from 'rxjs';
 import {Router} from '@angular/router';
 import { Injectable } from '@angular/core';
 import {catchError} from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private router:Router)
+    constructor(private router:Router,private toastr:ToastrService)
     {
 
     }
@@ -22,7 +23,14 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
         catchError(error=>{
             if(error){
-                if(error.status ===404){
+                if(error.status===400){
+                this.toastr.error(error.error.message,error.error.statusCode);
+
+                }
+                if(error.status ===401){
+                    this.toastr.error(error.error.message,error.error.statusCode);
+                }
+                if(error.status ===401){
                     this.router.navigateByUrl('/not-found');
                 }
                 if(error.state === 500){
