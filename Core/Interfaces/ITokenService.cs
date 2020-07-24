9a54faca4b0dce1978a297925eaba63c8d4a25ac
maharoz <mahruj66@gmail.com>
2020-07-24 19:50:36 +0600

    Adding a token generation service

---
+++ Core/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+   
+    }
+}
