eb0a8bb219028c2c22f2a5fedf3f411b61caf7c9
maharoz <mahruj66@gmail.com>
2020-07-24 11:55:50 +0600

    created identity classes

---
+++ Core/Entities/Identity/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class AppUser:IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+    }
+}
