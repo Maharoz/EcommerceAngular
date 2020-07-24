a3a6b51a2ba2900aa0adceda0365d3962b395e22
maharoz <mahruj66@gmail.com>
2020-07-24 13:52:31 +0600

    Added account controller

---
+++ Ecommerce/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Core.Entities;
+using Ecommerce.Dtos;
+using Ecommerce.Errors;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Controllers
+{
+    public class AccountController:BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManger,SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManger;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                DisplayName = user.DisplayName
+
+            };
+
+        }
+    }
+}
---
+++ Ecommerce/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Dtos
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+
+    }
+}
